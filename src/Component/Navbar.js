d050a9801bc3f0075ce0180e0600e7a498e6dbc2
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-06-28 16:16:11 +0200

    Add Styling for Navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,23 +3,23 @@ import '../index.css';
 
 export default function Navbar(){
     return (
-        <div>
-            <nav >
+        <div className="flex p-2 h-[10vh] align-center">
+            <nav className="flex w-[80%] m-auto pt-2 ml-[500]" >
                 <div>
-                    <ul>
-                        <p >Deckenbau GmbH</p>
-                        <li >
-                            <NavLink to="/">
+                    <ul className="flex items-center gap-[6vw] justify-around">
+                        <p className="text-[18px] font-extrabold text-black mr-[50px]">Deckenbau GmbH</p>
+                        <li className="text-[18px] font-bold">
+                            <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/">
                                 Home
                             </NavLink>
                         </li>
-                        <li >
-                            <NavLink to="/galerie">
+                        <li className="text-[18px] font-bold" >
+                            <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/galerie">
                                 Galerie
                             </NavLink>
                         </li>
-                        <li >
-                            <NavLink to="/ueberuns">
+                        <li  className="text-[18px] font-bold">
+                            <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/ueberuns">
                                 Über uns
                             </NavLink>
                         </li>
