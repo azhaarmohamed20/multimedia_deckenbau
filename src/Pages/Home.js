e1e8cc457e1451ee8933c3039f00ed6023de34c0
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-06-29 22:34:47 +0200

    Finish styling Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,25 @@
 import CarouselSlider from "../Component/CarouselSlider"
+import Homecontent from "../Component/HomeContent";
 import image1 from '../images/IMG_1.jpg';
 import image2 from '../images/IMG_2.jpg';
 import image3 from '../images/IMG_3.jpg';
 
-export default function Home(){
-
-    const images =[image1, image2, image3]
+export default function Home() {
+    const images = [image1, image2, image3];
     const texts = [
-        "Qualität und Präzision - Ihr Partner für professionelle Deckenbau-Lösungen", 
-        "Maßgeschneiderte Deckenbau-Lösungen für jeden Bedarf und jedes Budget.", 
-        "Innovative Techniken und Materialien für einzigartige Deckenkonzepte."];
-    return(
+      "Qualität und Präzision - Ihr Partner für professionelle Deckenbau-Lösungen",
+      "Maßgeschneiderte Deckenbau-Lösungen für jeden Bedarf und jedes Budget.",
+      "Innovative Techniken und Materialien für einzigartige Deckenkonzepte."
+    ];
+  
+    return (
         <>
-        <CarouselSlider images={images} texts={texts}/>
+            <div className="h-screen flex flex-col">
+                <div className="grow">
+                    <CarouselSlider images={images} texts={texts} />
+                </div>
+            </div>
+            <Homecontent />
         </>
-    )
-}
+    );
+  }
