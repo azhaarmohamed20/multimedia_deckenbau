c3fdcba7c6518422e67afcfc97b962b5cb73a8fd
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-06-29 18:13:57 +0200

    Fix Navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,9 +4,9 @@ import '../index.css';
 export default function Navbar(){
     return (
         <div className="pt-[0.5%]">
-            <nav className="flex m-auto pt-2 ml-[500]  h-[10vh] align-center" >
+            <nav className="flex m-auto pt-2 ml-[500] m-auto w-[85%] h-[10vh] align-center" >
                 <div>
-                    <ul className="flex items-center gap-[6vw] justify-around">
+                    <ul className="flex items-center gap-[10vw] justify-around">
                         <p className="text-[18px] font-extrabold text-black mr-[50px]">Deckenbau GmbH</p>
                         <li className="text-[18px] font-bold">
                             <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/">
