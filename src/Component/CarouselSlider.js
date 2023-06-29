4f36b1153f2e836faf3359ceda46d08bc1003771
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-06-29 22:34:27 +0200

    Fix Carousel

--- src/Component/CarouselSlider.js
+++ src/Component/CarouselSlider.js
@@ -32,37 +32,39 @@ export default function CarouselSlider({ images, texts }) {
   const currentText = texts[currentImageIndex];
 
   return (
-    <div
-      id="carousel-container"
-      className="absolute top-0 left-0 right-0 z-10 overflow-hidden"
-      style={{
-        height: `${window.innerHeight}px`,
-        backgroundImage: `url(${images[currentImageIndex]})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <button
-        className="absolute top-1/2 left-2 transform -translate-y-1/2 focus:outline-none"
-        onClick={prevImage}
-      >
-        <ChevronLeftIcon className="h-8 w-8 text-white" />
-      </button>
+    <>
+        <div
+        id="carousel-container"
+        className="absolute top-0 left-0 right-0 bottom-0 z-10 overflow-hidden"
+        style={{
+            backgroundImage: `url(${images[currentImageIndex]})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+        }}
+        >
+            <button
+                className="absolute top-1/2 left-2 transform -translate-y-1/2 focus:outline-none"
+                onClick={prevImage}
+            >
+                <ChevronLeftIcon className="h-8 w-8 text-white" />
+            </button>
 
-      <button
-        className="absolute top-1/2 right-2 transform -translate-y-1/2 focus:outline-none"
-        onClick={nextImage}
-      >
-        <ChevronRightIcon className="h-8 w-8 text-white" />
-      </button>
+            <button
+                className="absolute top-1/2 right-2 transform -translate-y-1/2 focus:outline-none"
+                onClick={nextImage}
+            >
+                <ChevronRightIcon className="h-8 w-8 text-white" />
+            </button>
 
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-        <p className="text-white font-bold text-[40px]">{currentText}</p>
-        <Link to="/ueberuns">
-            <button className="bg-white text-black px-4 py-2 mt-4 rounded-xl">Learn More</button>
-        </Link>
-    </div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                <p className="text-white font-bold text-[40px]">{currentText}</p>
+                <Link to="/ueberuns">
+                <button className="bg-white text-black px-4 py-2 mt-4 rounded-xl">Learn More</button>
+                </Link>
+            </div>
+        </div>
 
-    </div>
+    </>
+
   );
 }
