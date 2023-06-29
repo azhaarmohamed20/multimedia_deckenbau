f2d49d0f14caa57b5cc344691b3b342a3ba795ca
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-06-29 22:44:21 +0200

    Finish styling

--- src/Pages/Angebot.js
+++ src/Pages/Angebot.js
@@ -1,32 +1,37 @@
-import image1 from '../images/IMG_1.jpg';
-import image2 from '../images/IMG_2.jpg';
-import image3 from '../images/IMG_3.jpg';
+import image4 from '../images/IMG_0087.JPG';
+import image5 from '../images/IMG_0091.JPG';
+import image6 from '../images/IMG_0077.JPG';
+import image7 from '../images/IMG_0071.JPG';
 
 export default function Angebot() {
   return (
     <>
       <div className="absolute top-0 left-0 right-0 z-10 overflow-hidden h-[40vh] ">
-        <img src={image2} alt="Header" className="w-full h-auto object-cover object-center" />
+        <img src={image4} alt="Header" className="w-full h-auto object-cover object-center" />
       </div>
       <div className="p-auto relative m-auto w-[85%] bg-white z-20 mt-[28%] text-center">
         <h1 className='font-bold text-[40px]'>Montageprofis in jeder Situation </h1>
 
-        <div className="flex flex-row gap-[10vw]">
+        <div className="flex flex-row gap-[2vw]  m-auto">
             <div className="p-5 ">
-              <img src={image1} alt="Image 1" className="w-[350px]" />
-              <p>Beratung</p>
+              <img src={image5} alt="Image 1" className="w-[500px]" />
+              <p className='m-auto text-[24px]' >Metalldecken</p>
             </div>
 
             <div className="p-5">
-              <img src={image1} alt="Image 2" className="w-[350px]" />
-              <p>Planung</p>
+              <img src={image6} alt="Image 2" className="w-[500px]" />
+              <p className='m-auto text-[24px]' >Akustik</p>
             </div>
 
             <div className="p-5">
-              <img src={image1} alt="Image 3" className="w-[350px]" />
-              <p>Ausführung</p>
+              <img src={image7} alt="Image 3" className="w-[500px]" />
+              <p className='m-auto text-[24px]' >Beleuchtungg</p>
             </div>
         </div>
+        <br></br>
+        <p className='m-auto text-[24px]' >
+            Unser Leistungsspektrum umfasst eine präzise Montage aller Decken- und Akustiksysteme. Ausserdem sind wir spezialisiert für die Demontage, Wiedermontage und Anpassung bestehender Deckenbekleidungen, bei einer Änderung der Raumstruktur und bei der Optimierung von Einbaukomponenten wie z.B. Beleuchtungen, Klimaanlagen usw.
+        </p>
       </div>
     </>
   );
