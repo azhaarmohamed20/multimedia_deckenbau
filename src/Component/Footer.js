f098268c41c028ef5310035f4a633b654c01a72f
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-06-29 08:59:29 +0200

    Fix Footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,7 +4,7 @@ import '../index.css';
 export default function Footer(){
     return(
         <div>
-            <div className="flex m-auto h-16 fixed bottom-0 flex-row items-center gap-[8vw] align-center ">
+            <div className="flex m-auto h-16 relative bottom-0 flex-row items-center gap-[8vw] align-center ">
                 <p>Copyright Deckenbau GmbH</p>
                 <NavLink to="/impressum">Impressum</NavLink>
                 <NavLink to="/datenschutz">Datenschutz</NavLink>
