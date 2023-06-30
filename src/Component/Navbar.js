7fcb1bcd27019f47f14870906a87993c8b55b66c
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-06-30 08:55:07 +0200

    Fix Text Color in Navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -7,24 +7,24 @@ export default function Navbar(){
             <nav className="flex m-auto pt-2 ml-[500] m-auto w-[85%] h-[5vh] align-center" >
                 <div>
                     <ul className="flex items-center gap-[10vw] justify-around">
-                        <p className="text-[18px] font-extrabold text-black mr-[50px]">Deckenbau GmbH</p>
+                        <p className="text-[18px] font-extrabold text-gray-400 mr-[50px]">Deckenbau GmbH</p>
                         <li className="text-[18px] font-bold">
-                            <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/">
+                            <NavLink className="text-gray-400 aria-[current=page]:text-red-600 text-black" to="/">
                                 Home
                             </NavLink>
                         </li>
                         <li className="text-[18px] font-bold" >
-                            <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/angebot">
+                            <NavLink className="text-gray-400 aria-[current=page]:text-red-600 text-black" to="/angebot">
                                 Angebot
                             </NavLink>
                         </li>
                         <li  className="text-[18px] font-bold">
-                            <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/ueberuns">
+                            <NavLink className="text-gray-400 aria-[current=page]:text-red-600 text-black" to="/ueberuns">
                                 Über uns
                             </NavLink>
                         </li>
                         <li  className="text-[18px] font-bold">
-                            <NavLink className="text-black aria-[current=page]:text-red-600 text-black" to="/kontakt">
+                            <NavLink className="text-gray-400 aria-[current=page]:text-red-600 text-black" to="/kontakt">
                                 Kontakt
                             </NavLink>
                         </li>
